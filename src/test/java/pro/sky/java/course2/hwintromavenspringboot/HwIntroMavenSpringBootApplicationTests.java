d3bf05c409aaef59bce3605eb3d83d50b0f86a24
romax <maksimovskii.roman@yandex.ru>
2023-04-11 21:41:52 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/hwintromavenspringboot/HwIntroMavenSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hwintromavenspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwIntroMavenSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
